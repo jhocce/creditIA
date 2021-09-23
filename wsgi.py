6e3564520c29de7812d2bca3c47a3606e7edfe9b
jhocce <jhocce3022@hotmail.com>
2021-09-23 10:05:37 -0400

    v14

--- wsgi.py
+++ wsgi.py
@@ -8,12 +8,12 @@ import site
 # activate_this = '/home/aresqubitadmin/env/bin/activate_this.py'
 # execfile(activate_this, dict(__file__=activate_this))
 
-# site.addsitedir('/home/aresqubitadmin/env/lib/python3.6/site-packages')
+site.addsitedir('/home/aresqubitadmin/env/lib/python3.6/site-packages')
 
 sys.path.insert(0, '/var/www/creditIA')
 
+from .app import appas application
 
-from app import app as application
 
 
 # def application(environ, start_response):
