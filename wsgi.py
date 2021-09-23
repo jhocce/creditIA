298864d028ed47f34b325fa5d7a5ad0f1e95f7fd
jhocce <jhocce3022@hotmail.com>
2021-09-23 09:07:21 -0400

    v8

--- wsgi.py
+++ wsgi.py
@@ -3,10 +3,14 @@ import site
 
 # site.addsitedir('/home/aresqubitadmin/env/lib/python3.6/site-packages')
 
-sys.path.insert(0, '/var/www/creditIA')
+# sys.path.insert(0, '/var/www/creditIA')
 
+activate_this = '/home/aresqubitadmin/env/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
+
+# site.addsitedir('/home/aresqubitadmin/env/lib/python3.6/site-packages')
+
+sys.path.insert(0, '/var/www/creditIA')
 from app import app as application
 
 
-activate_this = '/home/aresqubitadmin/env/bin/activate_this.py'
-execfile(activate_this, dict(__file__=activate_this)
