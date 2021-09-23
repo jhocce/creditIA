4502d40f1f2acd1489aabfd6273c4199bdf3e0df
jhocce <jhocce3022@hotmail.com>
2021-09-23 10:33:20 -0400

    v20

--- wsgi.py
+++ wsgi.py
@@ -19,12 +19,12 @@ from app import app as application
 
 
 
-def application(environ, start_response):
-    status = '200 OK'
-    output = b'Hooray, mod_wsgi is working'
+# def application(environ, start_response):
+#     status = '200 OK'
+#     output = b'Hooray, mod_wsgi is working'
  
-    response_headers = [('Content-type', 'text/plain'),
-                        ('Content-Length', str(len(output)))]
-    start_response(status, response_headers)
+#     response_headers = [('Content-type', 'text/plain'),
+#                         ('Content-Length', str(len(output)))]
+#     start_response(status, response_headers)
  
-    return [output]
+#     return [output]
